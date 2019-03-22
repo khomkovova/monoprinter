8aaf7573b283e63bebeb93a7bd53d1da181e1fd4
khomkovova <khomko.vova@gmail.com>
2019-03-22 14:04:59 +0200

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,88 +1,66 @@
 
-#!/bin/bash
 
-for i in "$@"
-do
-case $i in
-    -gp=*|--git_password=*)
-    git_password="${i#*=}"
+FROM ubuntu:18.04
+FROM mysql
 
-    ;;
-    -gu=*|--git_username=*)
-    git_username="${i#*=}"
-    ;;
-    -aak=*|--aws_access_key_id=*)
-    aws_access_key_id="${i#*=}"
-    ;;
-    -asa=*|--aws_secret_access_key=*)
-    aws_secret_access_key="${i#*=}"
-    ;;
-    -ar=*|--aws_region=*)
-    aws_region="${i#*=}"
-    ;;
 
-    
-    *)
-          printf 'SET ALL OPTIONS:\n git_username \n git_password \n aws_access_key_id \n aws_secret_access_key \n aws_region \n'  # unknown option
-    ;;
-esac
-done
-if [ "$git_password" == "" ] 
-then 
-	echo "SET git_password"
-	exit 0
-fi
 
-if [ "$git_username" == "" ] 
-then 
-	echo "SET git_username"
-	exit 0
-fi
+# ARG git_username
+# ARG git_password
+# ARG aws_access_key_id
+# ARG aws_secret_access_key
+# ARG aws_region
 
-if [ "$aws_access_key_id" == "" ] 
-then 
-	echo "SET aws_access_key_id"
-	exit 0
-fi
 
-if [ "$aws_secret_access_key" == "" ] 
-then 
-	echo "SET aws_secret_access_key"
-	exit 0
-fi
+RUN apt-get update
+RUN apt-get install -y wget git gcc golang  mongodb
+RUN apt-get install -y redis-server
+RUN apt-get install -y python-pip
+RUN pip install awscli
 
-if [ "$aws_region" == "" ] 
-then 
-	echo "SET aws_region"
-	exit 0
-fi
+ENV GOPATH /go
+ENV PATH $GOPATH/bin:/usr/local/go/bin:$PATH
+RUN mkdir -p "$GOPATH/src" "$GOPATH/bin" && chmod -R 777 "$GOPATH"
+# ---------------------------------------------------------------------
+# RUN aws configure set aws_access_key_id $aws_access_key_id
+# RUN aws configure set aws_secret_access_key $aws_secret_access_key
+# RUN aws configure set region $aws_region
 
-# git_username
-# git_password
-# aws_access_key_id
-# aws_secret_access_key
-# aws_region
-echo "$@"
-aws configure set aws_access_key_id $aws_access_key_id 
-aws configure set aws_secret_access_key $aws_secret_access_key
-aws configure set region $aws_region
 
-service mongodb start
-service mysql start
-service --status-all
 
-# cd /go/src && git clone https://$git_username:$git_password@github.com/khomkovova/MonoPrinter.git 
-# cd /go/src/MonoPrinter 
-git clone https://$git_username:$git_password@github.com/khomkovova/MonoPrinterConfig.git
-ls -lah
-cp MonoPrinterConfig/liqpay_config.json liqpay/config.json
-cp MonoPrinterConfig/main_config.json config/config.json
-mysql_password=$(cat config/config.json |  python -c 'import json,sys;obj=json.load(sys.stdin);print obj["Databases"]["Mysql"]["Password"]')
-cat   ~/.aws/credentials
-aws s3 cp --recursive  s3://monoprinter/ . 
-ls -lah
-mongorestore --db monoprinter backup/mongodb/monoprinter
-# mysql -u root   < backup/mysql/monoprinter.sql
-# ls -lah
-# go build
-# exit 0
+# ENV git_username
+# ENV git_password
+# ENV aws_access_key_id
+# ENV aws_secret_access_key
+# ENV aws_region
+
+# RUN cd /go/src && git clone https://$git_username:$git_password@github.com/khomkovova/MonoPrinter.git 
+# WORKDIR /go/src/MonoPrinter 
+# RUN git clone https://$git_username:$git_password@github.com/khomkovova/MonoPrinterConfig.git
+# RUN ls -lah
+# RUN cp MonoPrinterConfig/liqpay_config.json liqpay/config.json
+# RUN cp MonoPrinterConfig/main_config.json config/config.json
+# RUN mysql_password=$(cat config/config.json |  python -c 'import json,sys;obj=json.load(sys.stdin);print obj["Databases"]["Mysql"]["Password"]')
+
+
+# RUN aws s3 cp --recursive  s3://monoprinter/ . 
+
+# RUN ls -lah
+# RUN mongorestore --db monoprinter backup/mongodb/monoprinter
+# RUN mysql -u root -p   < backup/mysql/monoprinter.sql
+
+# RUN go build 
+
+# ----------------------------------------------------------------------
+# ENTRYPOINT ["/bin/bash", "-c", "./llll$git_username"]
+
+
+
+
+CMD  ["/bin/bash"]
+ENTRYPOINT ["/bin/bash", "-c", "git clone https://$git_username:$git_password@github.com/khomkovova/MonoPrinter.git && cd MonoPrinter && chmod 744 ./install_all.sh  && ./install_all.sh --git_username=$git_username --git_password=$git_password --aws_access_key_id=$aws_access_key_id --aws_secret_access_key=$aws_secret_access_key --aws_region=$aws_region"]
+# WORKDIR /go/src/MonoPrinter
+# CMD ["/bin/bash", "-c", " "]
+# CMD ["/bin/bash", "-c", "echo 'asdas12345'"]
+
+
